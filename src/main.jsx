b0175da77707c9af35d119c2cf349af414a935ef
belw7898 <bella.wang.10@gmail.com>
2025-03-08 20:57:06 -0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 import App from "./App";
+=======
+//import './index.css'
+import App from "./pages/MainPage.jsx"
+>>>>>>> Stashed changes
 =======
 //import './index.css'
 import App from "./pages/MainPage.jsx"
