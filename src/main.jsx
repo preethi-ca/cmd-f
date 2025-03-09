4dce14ec131f8449f084ffff058b5f8a3a55fde5
preethi chidambaram <preethi.ca@outlook.com>
2025-03-08 21:07:36 -0800

    brought back old main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
-//import './index.css'
-import App from "./pages/MainPage.jsx"
+import App from "./App";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Router>
-      <App/>
-    </Router>
-  </StrictMode>,
-)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Router>
+    <App />
+  </Router>
+);
