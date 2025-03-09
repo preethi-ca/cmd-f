c42933ab90278404077992f669a020d761c3adf5
belw7898 <bella.wang.10@gmail.com>
2025-03-08 20:56:48 -0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
+<<<<<<< Updated upstream
 import App from "./App";
+=======
+//import './index.css'
+import App from "./pages/MainPage.jsx"
+>>>>>>> Stashed changes
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
